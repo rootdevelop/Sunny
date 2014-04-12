83f5fc983f4cf7faaebf91f68396b827e4809863
Xciles <Xciles@gmail.com>
2014-04-12 14:59:10 +0200

    Adding Controller for Media Adding Controller for Mission

---
+++ Web/Sunny.Services/Controllers/MediaController.cs
@@ -0,0 +1,129 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Sunny.Services.Data;
+using Sunny.Services.Domain;
+
+namespace Sunny.Services.Controllers
+{
+    public class MediaController : Controller
+    {
+        private readonly SunnyDatabaseContext _db = new SunnyDatabaseContext();
+
+        // GET: /Media/
+        public async Task<ActionResult> Index()
+        {
+            var medias = _db.Medias.Include(m => m.Mission);
+            return View(await medias.ToListAsync());
+        }
+
+        // GET: /Media/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Media media = await _db.Medias.FindAsync(id);
+            if (media == null)
+            {
+                return HttpNotFound();
+            }
+            return View(media);
+        }
+
+        // GET: /Media/Create
+        public ActionResult Create()
+        {
+            ViewBag.MissionId = new SelectList(_db.Missions, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Media/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="Id,ThumbnailUri,ImageUri,Description,Title,Date,Location,Tags,MissionId,MediaType")] Media media)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Medias.Add(media);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MissionId = new SelectList(_db.Missions, "Id", "Name", media.MissionId);
+            return View(media);
+        }
+
+        // GET: /Media/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Media media = await _db.Medias.FindAsync(id);
+            if (media == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MissionId = new SelectList(_db.Missions, "Id", "Name", media.MissionId);
+            return View(media);
+        }
+
+        // POST: /Media/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="Id,ThumbnailUri,ImageUri,Description,Title,Date,Location,Tags,MissionId,MediaType")] Media media)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(media).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MissionId = new SelectList(_db.Missions, "Id", "Name", media.MissionId);
+            return View(media);
+        }
+
+        // GET: /Media/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Media media = await _db.Medias.FindAsync(id);
+            if (media == null)
+            {
+                return HttpNotFound();
+            }
+            return View(media);
+        }
+
+        // POST: /Media/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Media media = await _db.Medias.FindAsync(id);
+            _db.Medias.Remove(media);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web/Sunny.Services/Controllers/MissionController.cs
@@ -0,0 +1,124 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Sunny.Services.Data;
+using Sunny.Services.Domain;
+
+namespace Sunny.Services.Controllers
+{
+    public class MissionController : Controller
+    {
+        private SunnyDatabaseContext db = new SunnyDatabaseContext();
+
+        // GET: /Mission/
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Missions.ToListAsync());
+        }
+
+        // GET: /Mission/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mission mission = await db.Missions.FindAsync(id);
+            if (mission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mission);
+        }
+
+        // GET: /Mission/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Mission/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="Id,Name,Date,Text,Location,Tags,ThumbnailUri,ImageUri")] Mission mission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Missions.Add(mission);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(mission);
+        }
+
+        // GET: /Mission/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mission mission = await db.Missions.FindAsync(id);
+            if (mission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mission);
+        }
+
+        // POST: /Mission/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="Id,Name,Date,Text,Location,Tags,ThumbnailUri,ImageUri")] Mission mission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mission).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(mission);
+        }
+
+        // GET: /Mission/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mission mission = await db.Missions.FindAsync(id);
+            if (mission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mission);
+        }
+
+        // POST: /Mission/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Mission mission = await db.Missions.FindAsync(id);
+            db.Missions.Remove(mission);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
