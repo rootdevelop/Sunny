0740491f3e4568430d578bd2a7afe3af6dfb9de0
Jeffrey Klink Logeman <jeffrey.klinklogeman@capgemini.com>
2014-04-12 17:31:24 +0200

    Added images - incons for Store app

--- App/Sunny.WindowsStore/Views/MissionView.xaml.cs
+++ App/Sunny.WindowsStore/Views/MissionView.xaml.cs
@@ -20,7 +20,7 @@ namespace Sunny.WindowsStore.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MissionView : Page
+    public sealed partial class MissionView : BaseView
     {
         public MissionView()
         {
