ec3eea56616b9f8c62de70e8849647a0db3e35cb
Xciles <Xciles@gmail.com>
2014-04-12 17:39:48 +0200

    Adding things

--- App/Sunny.Core/Domain/Mission.cs
+++ App/Sunny.Core/Domain/Mission.cs
@@ -13,6 +13,8 @@ namespace Sunny.Core.Domain
         public string Tags { get; set; }
         public string ThumbnailUri { get; set; }
         public string ImageUri { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
 
         public IList<Media> Media { get; set; }
     }
--- Web/Sunny.Services/Scripts/DatePicker.js
+++ Web/Sunny.Services/Scripts/DatePicker.js
@@ -1,5 +1,3 @@
 ﻿$(document).ready(function () {
-    $('.date-picker').datepicker({ autoclose: true }).next().on(ace.click_event, function () {
-        $(this).prev().focus();
-    });
+    $('.date-picker').datepicker({ autoclose: true });
 });
