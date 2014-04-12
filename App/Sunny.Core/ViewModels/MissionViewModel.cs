302d32eb87de6337201e8f998c61d3005e56585a
RootDevelop <robbert@rootdevelop.nl>
2014-04-12 15:15:01 +0200

    First setup for the ViewModels

--- App/Sunny.Core/ViewModels/MissionViewModel.cs
+++ App/Sunny.Core/ViewModels/MissionViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using Sunny.Core.ViewModels;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Sunny.Core
 {
     public class MissionViewModel : BaseViewModel
     {
+        public void Init(Domain.Mission mission)
+        {
+            Mission = mission;
+        }
+
         public MissionViewModel()
         {
+        }
+
+        private Domain.Mission _mission;
+
+        public Domain.Mission Mission
+        {
+            get
+            {
+                return _mission;
+            }
+            set
+            {
+                _mission = value;
+                RaisePropertyChanged(() => Mission);
+            }
+        }
+
+        private IList<Domain.Media> _mediaItems;
+
+        public IList<Domain.Media> MediaItems
+        {
+            get
+            {
+                return _mediaItems;
+            }
+            set
+            {
+                _mediaItems = value;
+                RaisePropertyChanged(() => MediaItems);
+            }
         }
     }
 }
