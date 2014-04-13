37d03d728268e6bebbf2f798595d5a91843ad271
Jeffrey Klink Logeman <jeffrey.klinklogeman@capgemini.com>
2014-04-13 02:26:02 +0200

    Navigation Store app

--- App/Sunny.WindowsStore/Views/AboutView.xaml.cs
+++ App/Sunny.WindowsStore/Views/AboutView.xaml.cs
@@ -20,7 +20,7 @@ namespace Sunny.WindowsStore.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AboutView : Page
+    public sealed partial class AboutView : BaseView
     {
         public AboutView()
         {
