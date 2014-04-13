b0fe9c4fc7ff6c2efca4fe8579cab0f7f087feee
Xciles <Xciles@gmail.com>
2014-04-13 03:01:49 +0200

    Adding livestream model

---
+++ App/Sunny.Core/ViewModels/LiveStreamViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace Sunny.Core.ViewModels
+{
+    public class LiveStreamViewModel : BaseViewModel
+    {
+        private string _userName;
+        private string _liveStreamUrl;
+
+        public string LiveStreamUrl
+        {
+            get { return _liveStreamUrl; }
+            set
+            {
+                _liveStreamUrl = value;
+                RaisePropertyChanged(() => LiveStreamUrl);
+            }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                RaisePropertyChanged(() => UserName);
+            }
+        }
+
+        public ICommand NewsDetailsCommand
+        {
+            get
+            {
+                return new MvxCommand<string>(message => {  });
+            }
+        }
+    }
+}
