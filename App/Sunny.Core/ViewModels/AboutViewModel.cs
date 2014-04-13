96b2528456f407f0ab5300028df0c2f1a4e8f89e
Jeffrey Klink Logeman <jeffrey.klinklogeman@capgemini.com>
2014-04-13 11:35:59 +0200

    Tuned the About page

--- App/Sunny.Core/ViewModels/AboutViewModel.cs
+++ App/Sunny.Core/ViewModels/AboutViewModel.cs
@@ -8,7 +8,12 @@ namespace Sunny.Core.ViewModels
     {
         public AboutViewModel()
         {
+            AboutProjectText = "This app was created as part of the Space Apps Challenge 2014 organized by NASA. In a period of 2 days we worked trough the night to develop the Sunny App that allows you to follow space missions, subscribe to them and watch livestreams while chatting with other viewers.";
+
+            AboutTechStuffText = "The app was created for iPad, Windows 8 & Android with Xamarin & MvvmCross. All of the code in this app is open-source!";
         }
+
+        public string AboutProjectText { get; set; }
+        public string AboutTechStuffText { get; set; }
     }
 }
-
